67439acf7da18dea09d33674d3558d1c1c6fec8e
Shreya Mooli <shreya.mooli@gmail.com>
2025-01-11 18:42:19 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print ("its me, do you remember?, im back")
+
+#list
+list1=[100,[56,78],900.56,"harry"]
+print(list1)
+
+#tuple
+tuple1=(79,"potter",7.9,9,34,90.765)
+print(tuple1)
+
+#dict
+dict1={23:98,"you":"mean","i":"good", "num":420,98:56.98}
+print(dict1)
